100426093321041232a995b5c8256802138c09d4
stamanor104 <stamanor104@gmail.com>
2024-09-17 07:58:22 +0000

    open skill code

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+//BRAND
+export const name = 'OpenSkill';
+export const company = 'SimonSolutions';
+export const websiteURL = 'http://localhost:3000';
+export const serverURL = 'http://localhost:5000';
+export const logo = 'https://firebasestorage.googleapis.com/v0/b/india-bd34b.appspot.com/o/artificial-intelligence.png?alt=media&token=2dc0da1c-12d0-4dd2-b908-cb8e57fcf3f0';
+export const razorpayEnabled = false;
+export const paypalEnabled = false;
+export const stripeEnabled = false;
+export const paystackEnabled = true;
+
+//PRICING :-
+
+//FREE 
+export const FreeType = 'Free Plan';
+export const FreeCost = 0;
+export const FreeTime = '';
+
+//MONTHLY 
+export const MonthType = 'Monthly Plan';
+export const MonthCost = 9;
+export const MonthTime = 'month';
+
+//YEARLY 
+export const YearType = 'Yearly Plan';
+export const YearCost = 99;
+export const YearTime = 'year';
+
+//TESTIMONIAL
+export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
+export const from = "Francis AK";
+export const profession = 'CEO at Nunya Solutions';
+export const photoURL = 'https://play-lh.googleusercontent.com/sV_ffBmBJt_je4RZHnfaCfcnL-Hy6C14Iol7H5EMj9fzI2GDOonuojdn5t9p6n9IAX8j';
+
+//PAYPAL
+export const paypalPlanIdOne = "P-1EM732768S920784HMWKW3OA";
+export const paypalPlanIdTwo = "P-8T744865W27080359MWOCE5Q";
+
+//RAZORPAY
+export const razorpayKeyId = "rzp_test_uqALjZHyTyuiOm";
+export const razorpayPlanIdOne = "plan_NMvvtDfznbRp6V";
+export const razorpayPlanIdTwo = "plan_NMRc9HARBQRLWA";
+
+//STRIPE
+export const stripePlanIdOne = "price_1OTo7CSDXmLtVnVeaHIHxqCj";
+export const stripePlanIdTwo = "price_1OTo7eSDXmLtVnVeBbn82U5B";
+
+//PAYSTACK
+export const paystackPlanIdOne = "PLN_ouqmm8eo6i2k9k8";
+export const paystackPlanIdTwo = "PLN_1v1xqb8io9t5lis";
+export const amountInZarOne = '170';
+export const amountInZarTwo = '1871';
